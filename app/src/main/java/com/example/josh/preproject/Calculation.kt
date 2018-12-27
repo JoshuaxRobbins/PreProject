64b9cf47e8e52ab2ce39f394f6c7a68916be0750
Joshua Robbins <joshua.robbins@mobileappscompany.us>
2018-12-27 14:06:33 -0500

    Experimenting with Koltin and Testing
    Signed-off-by: Joshua Robbins <joshua.robbins@mobileappscompany.us>

---
+++ app/src/main/java/com/example/josh/preproject/Calculation.kt
@@ -0,0 +1,11 @@
+package com.example.josh.preproject
+
+class Calculation {
+
+    companion object {
+        fun addNumbers(a: Int, b: Int):Int{
+            return (a+b)
+        }
+    }
+
+}
---
+++ app/src/test/java/com/example/josh/preproject/CalcTest.kt
@@ -0,0 +1,22 @@
+package com.example.josh.preproject
+
+import org.junit.Test
+import org.mockito.Mock
+
+class CalcTest {
+
+
+
+
+    @Test
+    fun testsWork1(){
+        var i = Calculation.addNumbers(10,10)
+        assert(i == 20)
+    }
+
+    @Test
+    fun testsWork2(){
+        var i = Calculation.addNumbers(9,10)
+        assert(i == 20)
+    }
+}
